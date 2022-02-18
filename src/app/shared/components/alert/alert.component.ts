8282e7912c6f4f2fd40998d54c66b56adf79dd60
David Barcenas <daveepro@outlook.com>
2022-02-17 20:03:29 -0600

    add alert success

--- src/app/shared/components/alert/alert.component.ts
+++ src/app/shared/components/alert/alert.component.ts
@@ -1,12 +1,20 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.component.html',
   styleUrls: ['./alert.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AlertComponent implements OnInit {
+  modalType = 'error';
+  message = '';
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (!this.message) {
+      this.message =
+        this.modalType === 'error' ? 'Ocurrió un error' : 'Operación exitosa';
+    }
+  }
 }
