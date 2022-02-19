50d7b1b2e960a6fc3af48856c7b85582f6312ec6
David Barcenas <daveepro@outlook.com>
2022-02-18 19:48:43 -0600

    change sidenav mod on small devices

--- src/app/layout/dashboard/dashboard.component.ts
+++ src/app/layout/dashboard/dashboard.component.ts
@@ -1,12 +1,42 @@
-import {Component, OnInit} from '@angular/core';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+import {Subject, takeUntil} from 'rxjs';
+
+import {MatDrawerMode} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DashboardComponent implements OnInit {
-  constructor() {}
+export class DashboardComponent implements OnInit, OnDestroy {
+  destroyed = new Subject<void>();
+  sideNavMode: MatDrawerMode = 'side';
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private cdr: ChangeDetectorRef,
+  ) {
+    this.breakpointObserver
+      .observe([Breakpoints.XSmall, Breakpoints.Small])
+      .pipe(takeUntil(this.destroyed))
+      .subscribe(res => {
+        this.cdr.markForCheck();
+        this.sideNavMode = res.matches ? 'over' : 'side';
+      });
+  }
 
   ngOnInit(): void {}
+
+  ngOnDestroy() {
+    this.destroyed.next();
+    this.destroyed.complete();
+  }
 }
