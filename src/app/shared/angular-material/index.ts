c1874d13376a8b4e0d73f1ce2f0916ae2956c6e7
David Barcenas <daveepro@outlook.com>
2022-02-22 21:42:07 -0600

    add buttons style

--- src/app/shared/angular-material/index.ts
+++ src/app/shared/angular-material/index.ts
@@ -12,6 +12,7 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatTableModule} from '@angular/material/table';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatRippleModule} from '@angular/material/core';
 
 export const MaterialModules = [
   LayoutModule,
@@ -28,4 +29,5 @@ export const MaterialModules = [
   MatTableModule,
   MatToolbarModule,
   MatPaginatorModule,
+  MatRippleModule,
 ];
