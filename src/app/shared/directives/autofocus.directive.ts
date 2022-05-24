d724cf36a63629c2ed596fe92d9a51648430529d
David Barcenas <daveepro@outlook.com>
2022-05-23 21:52:39 -0500

    add autofocus directive

---
+++ src/app/shared/directives/autofocus.directive.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectorRef, Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appAutofocus]'
+})
+export class AutofocusDirective {
+  constructor(private host: ElementRef, private cdr: ChangeDetectorRef) {}
+
+  ngAfterViewInit() {
+    this.host.nativeElement.focus();
+    this.cdr.detectChanges();
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MarkFormTouchedDirective } from './directives/mark-form-touched.directive';
+import { AutofocusDirective } from './directives/autofocus.directive';
 
 @NgModule({
-  declarations: [MarkFormTouchedDirective],
+  declarations: [MarkFormTouchedDirective, AutofocusDirective],
   imports: [
     CommonModule
   ]
