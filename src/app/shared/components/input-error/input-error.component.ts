697fc8bbd446fbda41f6c456663c566ffc1ae62a
David Barcenas <daveepro@outlook.com>
2022-05-26 20:36:13 -0500

    add error dictionary

--- src/app/shared/components/input-error/input-error.component.ts
+++ src/app/shared/components/input-error/input-error.component.ts
@@ -1,5 +1,6 @@
 import { Component, Host, Input, OnInit, Optional } from '@angular/core';
 import { FormGroupDirective } from '@angular/forms';
+import { ERROR_MESSAGES } from '../../utils/input-errors';
 
 @Component({
   selector: 'app-input-error',
@@ -33,85 +34,15 @@ export class InputErrorComponent implements OnInit {
     let firstError: string | null = null;
 
     if (control && this.formContainer.submitted) {
-      if (this.errors) {
-        Object.keys(this.errors).some(error => {
-          if (control.hasError(error)) {
-            firstError = this.errors[error];
-            return true;
-          }
-          return false;
-        });
-      }
+      const defaultErrors = this.errors || ERROR_MESSAGES;
+      Object.keys(defaultErrors).some(error => {
+        if (control.hasError(error)) {
+          firstError = defaultErrors[error];
+          return true;
+        }
+        return false;
+      });
     }
     return firstError;
   }
 }
-
-/*
- const control = this.formContainer.form.get(this.field);
-    const errors = control.errors;
-
-    if (!control || !errors) {
-      return null;
-    }
-
-    if (errors.required) {
-      return 'This field is required';
-    }
-
-    if (errors.minlength) {
-      return `This field must be at least ${errors.minlength.requiredLength} characters`;
-    }
-
-    if (errors.maxlength) {
-      return `This field must be at most ${errors.maxlength.requiredLength} characters`;
-    }
-
-    if (errors.pattern) {
-      return `This field must match the pattern ${errors.pattern.requiredPattern}`;
-    }
-
-    if (errors.min) {
-      return `This field must be at least ${errors.min.min}`;
-    }
-
-    if (errors.max) {
-      return `This field must be at most ${errors.max.max}`;
-    }
-
-    if (errors.email) {
-      return 'This field must be a valid email address';
-    }
-
-    if (errors.minDate) {
-      return `This field must be at least ${errors.minDate.minDate}`;
-    }
-
-    if (errors.maxDate) {
-      return `This field must be at most ${errors.maxDate.maxDate}`;
-    }
-
-    if (errors.minTime) {
-      return `This field must be at least ${errors.minTime.minTime}`;
-    }
-
-    if (errors.maxTime) {
-      return `This field must be at most ${errors.maxTime.maxTime}`;
-    }
-
-    if (errors.minItems) {
-      return `This field must contain at least ${errors.minItems.minItems} items`;
-    }
-
-    if (errors.maxItems) {
-      return `This field must contain at most ${errors.maxItems.maxItems} items`;
-    }
-
-    if (errors.uniqueItems) {
-      return `This field must contain unique items`;
-    }
-
-    if (errors.custom) {
-      return errors.
-  }  
-*/
---
+++ src/app/shared/utils/input-errors.ts
@@ -0,0 +1,16 @@
+export const ERROR_MESSAGES: Record<string, string> = {
+  required: 'This field is required',
+  email: 'This field must be a valid email address',
+  minlength: 'This field must be at least :placeholder characters',
+  maxlength: 'This field must be at most :placeholder characters',
+  pattern: 'This field must match the pattern :placeholder',
+  min: 'This field must be at least :placeholder',
+  max: 'This field must be at most :placeholder',
+  minDate: 'This field must be at least :placeholder',
+  maxDate: 'This field must be at most :placeholder',
+  minTime: 'This field must be at least :placeholder',
+  maxTime: 'This field must be at most :placeholder',
+  minItems: 'This field must contain at least :placeholder items',
+  maxItems: 'This field must contain at most :placeholder items',
+  uniqueItems: 'This field must contain unique items',
+};
