1506b69d2c74a27784e7f01d970898277c02cd40
David Barcenas <daveepro@outlook.com>
2022-05-27 19:54:50 -0500

    create login structure

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -12,7 +12,7 @@ export class LoginComponent implements OnInit {
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
       email: ['', Validators.required],
-      password: [''],
+      psswd: [''],
     });
   }
 
--- src/app/shared/directives/mark-form-touched.directive.ts
+++ src/app/shared/directives/mark-form-touched.directive.ts
@@ -2,7 +2,7 @@ import { Directive, HostListener, Self } from '@angular/core';
 import { ControlContainer } from '@angular/forms';
 
 @Directive({
-  selector: '[appMarkFormTouched]',
+  selector: '[markFormTouched]',
 })
 export class MarkFormTouchedDirective {
   constructor(@Self() private container: ControlContainer) {}
