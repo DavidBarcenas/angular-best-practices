5aef1a1b330ef46356387c2f48c792e3fda237d2
David Barcenas <daveepro@outlook.com>
2022-06-07 21:50:54 -0500

    create form submit directive

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { directives } from './directives';
 import { components } from './components';
+import { FormSubmitDirective } from './directives/form-submit.directive';
 
 @NgModule({
-  declarations: [directives, components],
+  declarations: [directives, components, FormSubmitDirective],
   imports: [CommonModule, ReactiveFormsModule],
   exports: [ReactiveFormsModule, directives, components],
 })
