209724fe6aad3c1980315f66e482622c2cbb6f39
David Barcenas <daveepro@outlook.com>
2022-07-24 22:11:15 -0500

    add email confirm component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -6,6 +6,7 @@ import { SharedModule } from '../shared/shared.module';
 import { AuthComponent } from './auth.component';
 import { VerifyCodeComponent } from './verify-code/verify-code.component';
 import { SignupComponent } from './signup/signup.component';
+import { EmailConfirmComponent } from './email-confirm/email-confirm.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { SignupComponent } from './signup/signup.component';
     AuthComponent,
     VerifyCodeComponent,
     SignupComponent,
+    EmailConfirmComponent,
   ],
   imports: [CommonModule, AuthRoutingModule, SharedModule],
 })
