58d253eb9d4fda5adce040ace38e74afa5a7d4a4
David Barcenas <daveepro@outlook.com>
2022-07-30 20:36:48 -0500

    start currency directive

---
+++ src/app/shared/directives/currency.directive.ts
@@ -0,0 +1,44 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[currency]',
+})
+export class CurrencyDirective {
+  constructor(private _inputEl: ElementRef) {}
+
+  @HostListener('keyup', ['$event'])
+  onInput() {
+    if (this._inputEl.nativeElement.value === '') return;
+    const commasRemoved = this._inputEl.nativeElement.value.replace(
+      /[^0-9.]/g,
+      '',
+    );
+
+    let toInt: number;
+    let toLocale: string;
+
+    if (commasRemoved.split('.').length > 1) {
+      const decimal = isNaN(parseFloat(commasRemoved.split('.')[1]))
+        ? ''
+        : parseInt(commasRemoved.split('.')[1]);
+      toInt = parseInt(commasRemoved);
+      toLocale =
+        toInt.toLocaleString('en-US') + '.' + decimal.toString().slice(0, 2);
+    } else {
+      toInt = parseInt(commasRemoved);
+      toLocale = toInt.toLocaleString('en-US');
+    }
+
+    const position = this._inputEl.nativeElement.selectionStart;
+
+    if (toLocale === 'NaN') {
+      this._inputEl.nativeElement.value = '';
+    } else {
+      this._inputEl.nativeElement.value = toLocale;
+    }
+
+    setTimeout(() => {
+      this._inputEl.nativeElement.setSelectionRange(position, position);
+    }, 0);
+  }
+}
--- src/app/shared/directives/index.ts
+++ src/app/shared/directives/index.ts
@@ -1,4 +1,9 @@
 import { AutofocusDirective } from './autofocus.directive';
+import { CurrencyDirective } from './currency.directive';
 import { MarkFormTouchedDirective } from './mark-form-touched.directive';
 
-export const directives = [MarkFormTouchedDirective, AutofocusDirective];
+export const directives = [
+  MarkFormTouchedDirective,
+  AutofocusDirective,
+  CurrencyDirective,
+];
