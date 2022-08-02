fa82531866a8387d85dd2fa37bf7494f3ad9cc10
David Barcenas <davidbarcenasmx@gmail.com>
2022-08-01 19:15:17 -0500

    add input currency component

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,3 +1,4 @@
 import { InputErrorComponent } from './input-error/input-error.component';
+import { InputCurrencyComponent } from './input-currency/input-currency.component';
 
-export const components = [InputErrorComponent];
+export const components = [InputErrorComponent, InputCurrencyComponent];
---
+++ src/app/shared/components/input-currency/input-currency.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-input-currency',
+  templateUrl: './input-currency.component.html',
+  styleUrls: ['./input-currency.component.scss'],
+})
+export class InputCurrencyComponent implements OnInit {
+  form = this.fb.group({
+    amount: '',
+  });
+  regex = /^\d{0,12}(\.\d{0,2}?$)/;
+  prevAmount = '';
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form.get('amount')?.valueChanges.subscribe(amount => {
+      if (!this.regex.test(amount)) {
+        this.form
+          .get('amount')
+          ?.setValue(this.prevAmount, { emitEvent: false });
+      } else {
+        this.prevAmount = amount;
+      }
+    });
+  }
+}
