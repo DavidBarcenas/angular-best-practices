1091e30f8269eb404f7bc1e3117d2fc00c4c8dd2
David Barcenas <davidbarcenasmx@gmail.com>
2022-11-07 11:56:20 -0600

    create standalone children

--- src/app/standalone/standalone.component.ts
+++ src/app/standalone/standalone.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DataTableComponent } from './data-table/data-table.component';
+import { SharedModule } from '../shared/shared.module';
 
 @Component({
   selector: 'app-standalone',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule, DataTableComponent],
   templateUrl: './standalone.component.html',
   styleUrls: ['./standalone.component.scss'],
 })
