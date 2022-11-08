73d28ffa4d7400e0b664fef87063251f23a7ab80
David Barcenas <davidbarcenasmx@gmail.com>
2022-11-08 16:12:10 -0600

    add header layout

--- src/app/reactive-dev/reactive-dev.module.ts
+++ src/app/reactive-dev/reactive-dev.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { ReactiveDevComponent } from './reactive-dev.component';
 import { ReactiveDevRoutingModule } from './reactive-dev.routing';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [ReactiveDevComponent],
-  imports: [CommonModule, ReactiveDevRoutingModule],
+  declarations: [ReactiveDevComponent, HomeComponent],
+  imports: [CommonModule, ReactiveDevRoutingModule, MatIconModule, MatButtonModule],
 })
 export class ReactiveDevModule {}
--- src/app/reactive-dev/reactive-dev.routing.ts
+++ src/app/reactive-dev/reactive-dev.routing.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { ReactiveDevComponent } from './reactive-dev.component';
 
-const routes: Routes = [{ path: '', component: ReactiveDevComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: ReactiveDevComponent,
+    children: [
+      { path: 'welcome', component: HomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
