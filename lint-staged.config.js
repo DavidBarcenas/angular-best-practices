a4b7a692c10b31d6ed08d23237c17b0186f39e0d
David Barcenas <davidbarcenasmx@gmail.com>
2022-12-09 16:53:42 -0600

    create navigation components and home style

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   '*.+(ts|js)': ['eslint --fix', 'prettier --write'],
-  '*.scss': ['stylelint --config ./.stylelintrc.json'],
+  '*.scss': ['stylelint --config ./.stylelintrc.json --fix']
 };
--- src/app/styling-components/styling-components.module.ts
+++ src/app/styling-components/styling-components.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarPrimaryComponent } from './components/navigation/navbar-primary/navbar-primary.component';
+import { NavbarSecondaryComponent } from './components/navigation/navbar-secondary/navbar-secondary.component';
 
 const routes: Routes = [{ path: '', component: HomeComponent }];
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, NavbarPrimaryComponent, NavbarSecondaryComponent],
   imports: [CommonModule, RouterModule.forChild(routes)]
 })
 export class StylingComponentsModule {}
