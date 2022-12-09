13f8d6f870826a02f06f4789a26a02fc45d99f02
David Barcenas <davidbarcenasmx@gmail.com>
2022-12-09 13:37:16 -0600

    add styling-components module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'styling-components',
+    loadChildren: () =>
+      import('./styling-components/styling-components.module').then(m => m.StylingComponentsModule)
+  },
+  { path: '', redirectTo: 'styling-components', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {}
