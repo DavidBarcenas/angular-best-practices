4f813069e8ea88c82209c8995b149c4eb6cb33de
David Barcenas <davidbarcenasmx@gmail.com>
2022-12-09 12:45:09 -0600

    update settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,31 +2,38 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
+    node: true
   },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    'plugin:prettier/recommended'
   ],
   overrides: [],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: './tsconfig.json',
+    project: './tsconfig.json'
   },
   plugins: ['@typescript-eslint', 'prettier'],
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    complexity: ['error', { max: 8 }],
     quotes: ['error', 'single', { avoidEscape: true }],
     semi: ['error', 'always'],
+    'max-depth': ['error', 4],
+    'no-nested-ternary': 'error',
+    'no-magic-numbers': [
+      'warn',
+      { detectObjects: false, enforceConst: true, ignore: [1], ignoreArrayIndexes: true }
+    ],
     'no-multiple-empty-lines': [
       'warn',
       {
         max: 2,
-        maxEOF: 1,
-      },
-    ],
-  },
+        maxEOF: 1
+      }
+    ]
+  }
 };
