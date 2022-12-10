d0ad6b82c03a1799d6d9ba79c5167f55ab5e832d
David Barcenas <davidbarcenasmx@gmail.com>
2022-12-09 18:37:54 -0600

    create thumbnail-list and promo component

--- src/app/styling-components/styling-components.module.ts
+++ src/app/styling-components/styling-components.module.ts
@@ -4,11 +4,19 @@ import { HomeComponent } from './home/home.component';
 import { RouterModule, Routes } from '@angular/router';
 import { NavbarPrimaryComponent } from './components/navigation/navbar-primary/navbar-primary.component';
 import { NavbarSecondaryComponent } from './components/navigation/navbar-secondary/navbar-secondary.component';
+import { PromoComponent } from './promo/promo.component';
+import { ThumbnailListComponent } from './components/content/thumbnail-list/thumbnail-list.component';
 
 const routes: Routes = [{ path: '', component: HomeComponent }];
 
 @NgModule({
-  declarations: [HomeComponent, NavbarPrimaryComponent, NavbarSecondaryComponent],
+  declarations: [
+    HomeComponent,
+    NavbarPrimaryComponent,
+    NavbarSecondaryComponent,
+    PromoComponent,
+    ThumbnailListComponent
+  ],
   imports: [CommonModule, RouterModule.forChild(routes)]
 })
 export class StylingComponentsModule {}
