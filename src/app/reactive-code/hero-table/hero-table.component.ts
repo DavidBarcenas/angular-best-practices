c10083790b1e4e98acc8aa9485d51d61276f5dae
David Barcenas <davidbarcenasmx@gmail.com>
2022-12-12 20:33:46 -0600

    create hero table layout

---
+++ src/app/reactive-code/hero-table/hero-table.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-table',
+  templateUrl: './hero-table.component.html',
+  styleUrls: ['./hero-table.component.scss']
+})
+export class HeroTableComponent {}
--- src/app/reactive-code/reactive-code.module.ts
+++ src/app/reactive-code/reactive-code.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroTableComponent } from './hero-table/hero-table.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HeroesComponent,
-    children: []
+    children: [
+      { path: 'all', component: HeroTableComponent },
+      { path: '', pathMatch: 'full', redirectTo: 'all' }
+    ]
   }
 ];
 
 @NgModule({
-  declarations: [HeroesComponent],
+  declarations: [HeroesComponent, HeroTableComponent],
   imports: [CommonModule, RouterModule.forChild(routes)]
 })
 export class ReactiveCodeModule {}
