616feb203dc4844a44493d07427a99ff1483fd90
David Barcenas <davidbarcenasmx@gmail.com>
2022-12-23 17:04:15 -0600

    create cart skeleton

--- src/app/reactive-ecommerce/cart/cart.component.ts
+++ src/app/reactive-ecommerce/cart/cart.component.ts
@@ -5,4 +5,23 @@ import { Component } from '@angular/core';
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
 })
-export class CartComponent {}
+export class CartComponent {
+  skeleton = this.buildSkeleton();
+
+  private buildSkeleton() {
+    return {
+      reviewTitle: { width: '16em', marginBottom: '2em' },
+      orderTitle: { width: '10em', marginBottom: '2em' },
+      img: { width: '4em', height: '4em', marginRight: '3em' },
+      product: { width: '8em', height: '.8em', display: 'block' },
+      desc: { width: '5em', height: '.5em' },
+      price: { width: '5em', height: '.8em', display: 'block', marginLeft: 'auto' },
+      quantity: { width: '8em', height: '.5em' },
+      total: { width: '5em', height: '.5em' },
+      amount: { width: '4em', height: '.8em' },
+      btn: { height: '2em', marginTop: '2em', borderRadius: '3em' },
+      reviewItems: Array(4),
+      orderItems: Array(5)
+    };
+  }
+}
