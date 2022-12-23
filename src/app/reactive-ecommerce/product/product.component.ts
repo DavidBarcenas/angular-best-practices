388b1fd6937789291f7628814bc3f4230b466d8c
David Barcenas <davidbarcenasmx@gmail.com>
2022-12-23 14:48:11 -0600

    add skeleton and display product data

--- src/app/reactive-ecommerce/product/product.component.ts
+++ src/app/reactive-ecommerce/product/product.component.ts
@@ -14,4 +14,34 @@ export class ProductComponent {
   product$ = this.productsService.product$(
     parseInt(this.route.snapshot.paramMap.get('id')! || '0')
   );
+
+  skeleton = this.buildSkeleton();
+
+  buildSkeleton() {
+    return [
+      { height: '2em', width: '80%' },
+      { height: '.9em', width: '60%' },
+      { height: '.9em', width: '50%' },
+      {
+        height: '1.5em',
+        width: '20%',
+        display: 'block',
+        margin: '2em 0'
+      },
+      {
+        height: '1.85em',
+        width: '20%',
+        borderRadius: '2em',
+        marginRight: '2em',
+        marginBottom: '2em',
+        count: 2
+      },
+      {
+        height: '3em',
+        width: '50%',
+        display: 'block',
+        count: 2
+      }
+    ];
+  }
 }
