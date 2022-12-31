06b149c068631873ba851f998fa1e9364cdf32a8
David Barcenas <davidbarcenasmx@gmail.com>
2022-12-30 22:38:05 -0600

    add title with vercel animation

--- src/app/advanced-rxjs/advanced-rxjs.component.ts
+++ src/app/advanced-rxjs/advanced-rxjs.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-advanced-rxjs',
-  templateUrl: './advanced-rxjs.component.html'
+  templateUrl: './advanced-rxjs.component.html',
+  styleUrls: ['./advanced-rxjs.component.scss']
 })
 export class AdvancedRxjsComponent {}
