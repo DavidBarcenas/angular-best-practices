bc87bda74f394b094fafc77544aa94efcd2d0cb2
David Barcenas <davidbarcenasmx@gmail.com>
2023-01-02 21:41:40 -0600

    multicast observables

--- src/app/advanced-rxjs/advanced-rxjs.component.ts
+++ src/app/advanced-rxjs/advanced-rxjs.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, Subject, map } from 'rxjs';
+
+interface UserResponse {
+  data: {
+    id: number;
+    email: string;
+    first_name: string;
+    last_name: string;
+    avatar: string;
+  };
+}
 
 @Component({
   template: '<h1>Advanced RXJS</h1>'
 })
 export class AdvancedRxjsComponent implements OnInit {
+  private http = inject(HttpClient);
+
   ngOnInit(): void {
-    this.unicast();
+    this.multicast();
   }
 
-  unicast() {
+  multicast(): void {
+    const user$ = this.http
+      .get<UserResponse>('https://reqres.in/api/users/2')
+      .pipe(map(response => response.data));
+
+    const userSubject = new Subject<UserResponse['data']>();
+    // We subscribe to the subject multiple times.
+    userSubject.subscribe(console.log);
+    userSubject.subscribe(console.log);
+
+    // Finally, we subscribe to our user Observable and pass along
+    // the notifications to the userSubject.
+    user$.subscribe(userSubject);
+  }
+
+  unicast(): void {
     const o = new Observable(() => {
       console.log('new subscriber');
       return () => null;
