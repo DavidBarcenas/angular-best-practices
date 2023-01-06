6161a3de1c71a05bc0c157d6175be50b4fe21d1a
David Barcenas <davidbarcenasmx@gmail.com>
2023-01-05 19:55:33 -0600

    start with multicast operator

--- src/app/advanced-rxjs/advanced-rxjs.component.ts
+++ src/app/advanced-rxjs/advanced-rxjs.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, Subject, map } from 'rxjs';
+import { Observable, Subject, connectable, interval, map, takeUntil } from 'rxjs';
 
 interface UserResponse {
   data: {
@@ -19,9 +19,34 @@ export class AdvancedRxjsComponent implements OnInit {
   private http = inject(HttpClient);
 
   ngOnInit(): void {
-    this.multicast();
+    this.multicastOperator();
   }
 
+  // We'll use connectable operator to create multicasted Observables.
+  multicastOperator(): void {
+    const completeSubject = new Subject<boolean>();
+    // we create The source observable
+    const multicasted = connectable(interval(1000).pipe(takeUntil(completeSubject)), {
+      connector: () => new Subject<number>()
+    });
+
+    // We subscribe to the observable multiple times.
+    multicasted.subscribe(console.log);
+    multicasted.subscribe(console.log);
+
+    // Here we connect all consumers to the source
+    multicasted.connect();
+
+    // we stop the interval and stop emitting data
+    setTimeout(() => {
+      completeSubject.next(true);
+      completeSubject.complete();
+    }, 5000);
+  }
+
+  // When a Subject receives a new Observer it does
+  // not invoke a new execution for the production of values.
+  // Rather, each observer receives the same produced values.
   multicast(): void {
     const user$ = this.http
       .get<UserResponse>('https://reqres.in/api/users/2')
@@ -37,6 +62,7 @@ export class AdvancedRxjsComponent implements OnInit {
     user$.subscribe(userSubject);
   }
 
+  // Each Observer owns a separate execution of the Observable.
   unicast(): void {
     const o = new Observable(() => {
       console.log('new subscriber');
