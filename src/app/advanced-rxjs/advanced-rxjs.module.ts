7fc244c3ed16bce4cf8a6d55a48d86b44d76a7c0
David Barcenas <davidbarcenasmx@gmail.com>
2023-01-10 18:57:47 -0600

    separate subjects into their own file

--- src/app/advanced-rxjs/advanced-rxjs.module.ts
+++ src/app/advanced-rxjs/advanced-rxjs.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdvancedRxjsComponent } from './advanced-rxjs.component';
 import { RouterModule, Routes } from '@angular/router';
+import { SubjectComponent } from './subject.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: AdvancedRxjsComponent
+    component: AdvancedRxjsComponent,
+    children: [
+      { path: 'subjects', component: SubjectComponent },
+      { path: '', redirectTo: 'subject', pathMatch: 'full' }
+    ]
   }
 ];
 
 @NgModule({
-  declarations: [AdvancedRxjsComponent],
+  declarations: [AdvancedRxjsComponent, SubjectComponent],
   imports: [CommonModule, RouterModule.forChild(routes)]
 })
 export class AdvancedRxjsModule {}
