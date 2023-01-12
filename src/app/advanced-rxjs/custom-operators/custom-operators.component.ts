a357e7a6f783e4543ea2077e101fceb066de3f92
David Barcenas <davidbarcenasmx@gmail.com>
2023-01-12 16:48:27 -0600

    create custom logger pipe

--- src/app/advanced-rxjs/custom-operators/custom-operators.component.ts
+++ src/app/advanced-rxjs/custom-operators/custom-operators.component.ts
@@ -1,10 +1,67 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectionStrategy, Component, OnInit, inject, isDevMode } from '@angular/core';
+import { Observable, UnaryFunction, map, pipe, tap } from 'rxjs';
+
+interface UserResponse {
+  page: number;
+  per_page: number;
+  total: number;
+  total_pages: number;
+  data: {
+    id: number;
+    email: string;
+    first_name: string;
+    last_name: string;
+    avatar: string;
+  }[];
+}
+
+enum LoggerType {
+  Count,
+  Debug,
+  Dir,
+  Log,
+  Table
+}
 
 @Component({
   templateUrl: './custom-operators.component.html',
   styleUrls: ['./custom-operators.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CustomOperatorsComponent {
+export class CustomOperatorsComponent implements OnInit {
+  private http = inject(HttpClient);
   buttons = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  ngOnInit(): void {
+    this.http
+      .get<UserResponse>('https://reqres.in/api/users')
+      .pipe(
+        map(response => response.data),
+        this.logger(),
+        this.logger(LoggerType.Table)
+      )
+      .subscribe();
+  }
+
+  // create custom operator
+  logger(loggerType = LoggerType.Log): UnaryFunction<Observable<any>, Observable<any>> {
+    return pipe(tap(this.getLoggerByType(loggerType)));
+  }
+
+  private getLoggerByType(loggerType: LoggerType): (value: any) => void {
+    // We list the types of console that we will use
+    const loggerByType = {
+      [LoggerType.Count]: console.count,
+      [LoggerType.Debug]: console.debug,
+      [LoggerType.Dir]: console.dir,
+      [LoggerType.Log]: console.log,
+      [LoggerType.Table]: console.table
+    };
+    // If we are developing it will return the log,
+    // otherwise it will not return anything
+    return isDevMode() ? loggerByType[loggerType] : () => {};
+  }
 }
