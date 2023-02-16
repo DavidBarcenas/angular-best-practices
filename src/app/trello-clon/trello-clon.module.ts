bd22114daf3499917dacad986fc3f18dbab7eba1
David Barcenas <dbarcenas.dev@gmail.com>
2023-02-15 21:39:32 -0600

    trello-clone: implement overlay from cdk

--- src/app/trello-clon/trello-clon.module.ts
+++ src/app/trello-clon/trello-clon.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { OverlayModule } from '@angular/cdk/overlay';
+
 import { TrelloClonRoutingModule } from './trello-clon-routing.module';
 import { TrelloClonComponent } from './trello-clon.component';
 import { LoginComponent } from './pages/login/login.component';
 import { BtnComponent } from './components/btn/btn.component';
+import { BoardsComponent } from './pages/boards/boards.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [TrelloClonComponent, LoginComponent, BtnComponent],
-  imports: [CommonModule, TrelloClonRoutingModule]
+  declarations: [
+    TrelloClonComponent,
+    LoginComponent,
+    BtnComponent,
+    BoardsComponent,
+    NavbarComponent
+  ],
+  imports: [CommonModule, TrelloClonRoutingModule, OverlayModule]
 })
 export class TrelloClonModule {}
