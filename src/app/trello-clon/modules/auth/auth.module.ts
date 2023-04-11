081e603e88db9b3ce46441f40030cbc5a10e70e4
David Barcenas <dbarcenas.dev@gmail.com>
2023-04-10 21:48:49 -0600

    add auth layout structure

--- src/app/trello-clon/modules/auth/auth.module.ts
+++ src/app/trello-clon/modules/auth/auth.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthComponent } from './pages/auth/auth.component';
 import { AuthRoutingModule } from './auth-routing.module';
+import { HomeBackgroundComponent } from './components/home-background/home-background.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AuthComponent],
+  declarations: [AuthComponent, HomeBackgroundComponent, HeaderComponent, FooterComponent],
   imports: [CommonModule, AuthRoutingModule]
 })
 export class AuthModule {}
