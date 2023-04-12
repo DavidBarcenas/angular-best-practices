3f5c6f40120aa0e0248b9a2685474dc41ce25705
David Barcenas <dbarcenas.dev@gmail.com>
2023-04-11 21:17:19 -0600

    add login form

--- src/app/trello-clon/modules/auth/pages/login/login.component.ts
+++ src/app/trello-clon/modules/auth/pages/login/login.component.ts
@@ -1,7 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { faEye, faEyeSlash, faPen, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html'
 })
-export class LoginComponent {}
+export class LoginComponent {
+  private fb: FormBuilder = inject(FormBuilder);
+
+  protected readonly faPen = faPen;
+  protected readonly faEye = faEye;
+  protected readonly faEyeSlash = faEyeSlash;
+  protected readonly faSpinner = faSpinner;
+
+  showPassword = false;
+  status = 'idle';
+
+  form = this.fb.nonNullable.group({
+    email: ['', [Validators.email, Validators.required]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
+
+  login() {
+    this.form.markAllAsTouched();
+
+    if (this.form.valid) {
+      this.status = 'loading';
+      const { email, password } = this.form.getRawValue();
+    }
+  }
+}
