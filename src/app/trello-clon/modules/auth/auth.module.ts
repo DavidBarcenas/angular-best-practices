4d24b8c5dcd31418804a94565c62b25c7d592c7a
David Barcenas <dbarcenas.dev@gmail.com>
2023-04-11 21:18:49 -0600

    update auth component layout

--- src/app/trello-clon/modules/auth/auth.module.ts
+++ src/app/trello-clon/modules/auth/auth.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AuthComponent } from './pages/auth/auth.component';
 import { AuthRoutingModule } from './auth-routing.module';
 import { HomeBackgroundComponent } from './components/home-background/home-background.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LoginComponent } from './pages/login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,6 +17,6 @@ import { LoginComponent } from './pages/login/login.component';
     FooterComponent,
     LoginComponent
   ],
-  imports: [CommonModule, AuthRoutingModule]
+  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule, FontAwesomeModule]
 })
 export class AuthModule {}
