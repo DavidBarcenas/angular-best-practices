62332aa2b77b5bddff8ce8606521639bbb515f89
David Barcenas <dbarcenas.dev@gmail.com>
2023-04-16 21:02:03 -0600

    implement register service

--- src/app/trello-clon/modules/auth/pages/register/register.component.ts
+++ src/app/trello-clon/modules/auth/pages/register/register.component.ts
@@ -2,6 +2,10 @@ import { Component, inject } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { CustomValidators } from '../../../../shared/utils/validators';
 import { faEye, faEyeSlash, faPen } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { RequestStatus } from '../../../../models/request-status';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-register',
@@ -9,6 +13,16 @@ import { faEye, faEyeSlash, faPen } from '@fortawesome/free-solid-svg-icons';
 })
 export class RegisterComponent {
   private fb: FormBuilder = inject(FormBuilder);
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  protected readonly faPen = faPen;
+  protected readonly faEyeSlash = faEyeSlash;
+  protected readonly faEye = faEye;
+
+  formValidateEmail = this.fb.nonNullable.group({
+    email: ['', [Validators.email, Validators.required]]
+  });
 
   form = this.fb.nonNullable.group(
     {
@@ -23,18 +37,43 @@ export class RegisterComponent {
   );
 
   showPassword = false;
-  status = 'idle';
+  status: RequestStatus = 'idle';
+  statusValidateEmail: RequestStatus = 'idle';
 
   register() {
     this.form.markAllAsTouched();
     if (this.form.valid) {
       this.status = 'loading';
       const { name, email, password } = this.form.getRawValue();
-      console.log(name, email, password);
+      this.authService
+        .register(name, email, password)
+        .pipe(switchMap(() => this.authService.login(email, password)))
+        .subscribe({
+          next: () => {
+            this.status = 'success';
+            this.router.navigate(['/trello-clone/dashboard']);
+          },
+          error: () => (this.status = 'error')
+        });
     }
   }
 
-  protected readonly faPen = faPen;
-  protected readonly faEyeSlash = faEyeSlash;
-  protected readonly faEye = faEye;
+  validateEmail() {
+    this.formValidateEmail.markAllAsTouched();
+    if (this.formValidateEmail.valid) {
+      this.statusValidateEmail = 'loading';
+      const { email } = this.formValidateEmail.getRawValue();
+      this.authService.isAvailable(email).subscribe({
+        next: ({ isAvailable }) => {
+          if (isAvailable) {
+            this.statusValidateEmail = 'success';
+            this.form.get('email')?.setValue(email);
+          } else {
+            this.statusValidateEmail = 'error';
+            this.router.navigate(['/trello-clone/login'], { queryParams: { email } });
+          }
+        }
+      });
+    }
+  }
 }
