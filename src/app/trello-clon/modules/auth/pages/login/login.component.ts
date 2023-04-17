9e0c67831d47b935907678eea5b9b2619b8f8728
David Barcenas <dbarcenas.dev@gmail.com>
2023-04-16 21:01:47 -0600

    implement login service

--- src/app/trello-clon/modules/auth/pages/login/login.component.ts
+++ src/app/trello-clon/modules/auth/pages/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component, inject } from '@angular/core';
 import { faEye, faEyeSlash, faPen, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import { FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RequestStatus } from '../../../../models/request-status';
 
 @Component({
   selector: 'app-login',
@@ -8,6 +11,9 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class LoginComponent {
   private fb: FormBuilder = inject(FormBuilder);
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  private activatedRoute = inject(ActivatedRoute);
 
   protected readonly faPen = faPen;
   protected readonly faEye = faEye;
@@ -15,12 +21,23 @@ export class LoginComponent {
   protected readonly faSpinner = faSpinner;
 
   showPassword = false;
-  status = 'idle';
+  status: RequestStatus = 'idle';
 
   form = this.fb.nonNullable.group({
     email: ['', [Validators.email, Validators.required]],
     password: ['', [Validators.required, Validators.minLength(6)]]
   });
+
+  constructor() {
+    this.activatedRoute.queryParamMap.subscribe({
+      next: params => {
+        const email = params.get('email');
+        if (email) {
+          this.form.get('email')?.setValue(email);
+        }
+      }
+    });
+  }
 
   login() {
     this.form.markAllAsTouched();
@@ -28,6 +45,15 @@ export class LoginComponent {
     if (this.form.valid) {
       this.status = 'loading';
       const { email, password } = this.form.getRawValue();
+      this.authService.login(email, password).subscribe({
+        next: () => {
+          this.status = 'success';
+          this.router.navigate(['/trello-clone/dashboard']);
+        },
+        error: () => {
+          this.status = 'error';
+        }
+      });
     }
   }
 }
