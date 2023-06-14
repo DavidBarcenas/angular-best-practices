01403563804e6fdb59a0fb1d2f2a86f300c4118d
David Barcenas <dbarcenas.dev@gmail.com>
2023-06-13 19:11:31 -0600

    feat(signals): add age calculator component

---
+++ signals/src/app/age-calculator/age-calculator.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-age-calculator',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './age-calculator.component.html',
+  styles: [
+  ]
+})
+export class AgeCalculatorComponent {
+
+}
--- signals/src/app/app.component.ts
+++ signals/src/app/app.component.ts
@@ -1,61 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
-interface Technology {
-  name: string;
-  url: string;
-  image: string;
-}
+import { AgeCalculatorComponent } from './age-calculator/age-calculator.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, AgeCalculatorComponent],
   templateUrl: './app.component.html',
   styles: [],
 })
-export class AppComponent {
-  techList: Technology[] = [
-    {
-      name: 'Angular 16',
-      url: 'https://angular.io/',
-      image: 'assets/icons/angular-icon.svg'
-    },
-    {
-      name: 'Conventional commits',
-      url: 'https://www.conventionalcommits.org/en/v1.0.0/',
-      image: 'assets/icons/commits-icon.png'
-    },
-    {
-      name: 'ESlint',
-      url: 'https://eslint.org/',
-      image: 'assets/icons/eslint-icon.svg'
-    },
-    {
-      name: 'Husky',
-      url: 'https://typicode.github.io/husky/',
-      image: 'assets/icons/husky-icon.png'
-    },
-    {
-      name: 'pNpm',
-      url: 'https://pnpm.io/es/',
-      image: 'assets/icons/pnpm-icon.svg'
-    },
-    {
-      name: 'Prettier',
-      url: 'https://prettier.io/',
-      image: 'assets/icons/prettier-icon.svg'
-    },
-    {
-      name: 'Sass',
-      url: 'https://sass-lang.com/',
-      image: 'assets/icons/sass-icon.svg'
-    },
-    {
-      name: 'Tailwindcss',
-      url: 'https://tailwindcss.com/',
-      image: 'assets/icons/tailwindcss-icon.svg'
-    },
-  ]
-}
+export class AppComponent {}
