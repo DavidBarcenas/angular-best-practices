d66eaf56f9db1c6ac9eb753520efdb3238f88196
David Barcenas <dbarcenas.dev@gmail.com>
2023-06-15 19:22:55 -0600

    chore(marvel-heroes): update environments

--- marvel-heroes/src/environments/environment.development.ts
+++ marvel-heroes/src/environments/environment.development.ts
@@ -1,1 +1,7 @@
-export const environment = {};
+import { MARVEL_API_KEY, MARVEL_API_PRIVATE_KEY, MARVEL_API_URL } from './env';
+
+export const environment = {
+  marvelAPI: MARVEL_API_URL,
+  marvelAPIPublicKey: MARVEL_API_KEY,
+  marvelAPIPrivateKey: MARVEL_API_PRIVATE_KEY,
+};
