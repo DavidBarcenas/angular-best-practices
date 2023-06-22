13dbf7895aa19499f9067deb96456927c8d595ea
David Barcenas <dbarcenas.dev@gmail.com>
2023-06-21 20:58:22 -0600

    feat(forms): create template forms component

---
+++ forms/src/app/template-forms/template-forms-page/template-forms-page.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-template-forms-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './template-forms-page.component.html',
+  styles: [
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TemplateFormsPageComponent {
+
+}
