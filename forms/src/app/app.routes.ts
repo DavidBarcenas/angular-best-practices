8626e21908795089151e267a8352377f5479482f
David Barcenas <dbarcenas.dev@gmail.com>
2023-06-22 20:37:13 -0600

    feat(forms): create filters component

--- forms/src/app/app.routes.ts
+++ forms/src/app/app.routes.ts
@@ -2,11 +2,17 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'template-driven',
     title: 'Template-Driven Forms Playground',
     loadComponent: () =>
       import('./template-forms/template-forms-page/template-forms-page.component').then(
         (c) => c.TemplateFormsPageComponent
       ),
   },
+  {
+    path: 'filters',
+    title: 'Advanced Filters Playground',
+    loadComponent: () => import('./filters/filters.component').then((c) => c.FiltersComponent),
+  },
+  { path: '', redirectTo: 'filters', pathMatch: 'full' },
 ];
