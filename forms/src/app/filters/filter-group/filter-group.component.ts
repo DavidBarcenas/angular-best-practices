8fc620e6274da3d6bbd5ae2e09f193277958c4e4
David Barcenas <dbarcenas.dev@gmail.com>
2023-06-24 19:10:10 -0600

    feat(forms): connect view and controller

--- forms/src/app/filters/filter-group/filter-group.component.ts
+++ forms/src/app/filters/filter-group/filter-group.component.ts
@@ -1,15 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgxSliderModule, Options } from 'ngx-slider-v2';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-filter-group',
   standalone: true,
-  imports: [CommonModule, NgxSliderModule],
+  imports: [CommonModule, NgxSliderModule, FormsModule],
   templateUrl: './filter-group.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FilterGroupComponent {
+  @Output('activeFilters') activeFilters = new EventEmitter<any>();
   ratingOptions: Options = {
     floor: 0,
     ceil: 10,
@@ -18,7 +20,74 @@ export class FilterGroupComponent {
     floor: 2000,
     ceil: 2023,
   };
-  movieGenres = ['Action', 'Horror', 'Comedy', 'Drama', 'Fantasy', 'Adventure', 'Classics'];
-  languages = ['English', 'French', 'German', 'Spanish', 'Italian', 'Russian'];
+  movieGenres = [
+    { name: 'action', checked: false },
+    { name: 'horror', checked: false },
+    {
+      name: 'comedy',
+      checked: false,
+    },
+    { name: 'drama', checked: false },
+    { name: 'fantasy', checked: false },
+    {
+      name: 'adventure',
+      checked: false,
+    },
+    { name: 'classic', checked: false },
+  ];
+  languages = [
+    { name: 'english', checked: false },
+    { name: 'french', checked: false },
+    {
+      name: 'german',
+      checked: false,
+    },
+    { name: 'spanish', checked: false },
+    { name: 'italian', checked: false },
+    { name: 'russian', checked: false },
+  ];
   orderList = ['new', 'featured'];
+
+  filters: any = {
+    query: '',
+    languages: [],
+    genres: [],
+    orderBy: '',
+    rating: {
+      min: 0,
+      max: 10,
+    },
+    date: {
+      min: 2000,
+      max: 2023,
+    },
+  };
+
+  onFiltersChange() {
+    this.filters.languages = this.languages.filter((l) => l.checked).map((l) => l.name);
+    this.filters.genres = this.movieGenres.filter((g) => g.checked).map((g) => g.name);
+    console.log(this.cleanEmptyProperties({ ...this.filters }));
+    this.activeFilters.emit(this.cleanEmptyProperties({ ...this.filters }));
+  }
+
+  cleanEmptyProperties(obj: any) {
+    for (let prop in obj) {
+      if (obj[prop] === null || obj[prop] === undefined || obj[prop] === '') {
+        delete obj[prop];
+      } else if (typeof obj[prop] === 'object') {
+        if (Array.isArray(obj[prop])) {
+          obj[prop] = obj[prop].filter((value: any) => value !== '');
+          if (obj[prop].length === 0) {
+            delete obj[prop];
+          }
+        } else {
+          this.cleanEmptyProperties(obj[prop]);
+          if (Object.keys(obj[prop]).length === 0) {
+            delete obj[prop];
+          }
+        }
+      }
+    }
+    return obj;
+  }
 }
--- forms/src/app/filters/filters.component.ts
+++ forms/src/app/filters/filters.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FilterGroupComponent } from './filter-group/filter-group.component';
 import { DataTableComponent } from './data-table/data-table.component';
+import { movieListMock } from './mock-data';
 
 @Component({
   selector: 'app-filters',
@@ -11,4 +12,32 @@ import { DataTableComponent } from './data-table/data-table.component';
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FiltersComponent {}
+export class FiltersComponent {
+  movies = movieListMock;
+
+  filterData(filters: any) {
+    this.movies = movieListMock.filter((movie: any) => {
+      for (const filtro in filters) {
+        if (!filters[filtro]) continue;
+
+        if (filtro === 'date') {
+          const { min, max } = filters[filtro];
+          if (movie.date < min || movie.date > max) return false;
+        } else if (filtro === 'genres') {
+          if (!filters[filtro].includes(movie.genre.toLowerCase())) return false;
+        } else if (filtro === 'languages') {
+          if (!filters[filtro].includes(movie.language.toLowerCase())) return false;
+        } else if (filtro === 'query') {
+          if (!movie.title.toLowerCase().includes(filters[filtro].toLowerCase())) return false;
+        } else if (filtro === 'orderBy') {
+          if (movie.status.toLowerCase() !== filters[filtro].toLowerCase()) return false;
+        } else {
+          if (filters[filtro].toString().toLowerCase() !== filters[filtro].toString().toLowerCase()) {
+            return false;
+          }
+        }
+      }
+      return true;
+    });
+  }
+}
