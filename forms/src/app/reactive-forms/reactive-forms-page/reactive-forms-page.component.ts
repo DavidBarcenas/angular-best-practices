126cca063b82eb11b459661475f91594ca2bcfe8
David Barcenas <dbarcenas.dev@gmail.com>
2023-07-08 22:33:18 -0600

    feat(reactive-forms): add form layout

--- forms/src/app/reactive-forms/reactive-forms-page/reactive-forms-page.component.ts
+++ forms/src/app/reactive-forms/reactive-forms-page/reactive-forms-page.component.ts
@@ -1,15 +1,30 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './reactive-forms-page.component.html',
   styles: [
+    `
+      .form-label {
+        @apply block text-sm font-medium text-gray-700;
+      }
+
+      .form-input {
+        @apply mt-1 w-full rounded-md border border-gray-400 bg-white text-sm text-gray-700 shadow-sm py-2 px-3;
+      }
+    `,
   ],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ReactiveFormsPageComponent {
-
+  get years() {
+    const now = new Date().getUTCFullYear();
+    return Array(now - (now - 40))
+      .fill('')
+      .map((_, idx) => now - idx);
+  }
 }
