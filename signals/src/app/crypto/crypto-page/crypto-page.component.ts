72c7fbeb69ad17f1ace786452cd28cc3abe8e7d7
David Barcenas <dbarcenas.dev@gmail.com>
2023-07-29 20:17:08 -0600

    feat(crypto): implement mocks

--- signals/src/app/crypto/crypto-page/crypto-page.component.ts
+++ signals/src/app/crypto/crypto-page/crypto-page.component.ts
@@ -1,14 +1,52 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BehaviorSubject, mergeMap, Observable, of, shareReplay } from 'rxjs';
+import { PriceApiService } from '../price-api.service';
+import { MockPriceApiService } from '../mock-price-api.service';
+
+// in a real app could be environment.useMocks ?
+const useMocks = true;
 
 @Component({
   selector: 'app-crypto-page',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './crypto-page.component.html',
-  styles: [
-  ]
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    {
+      provide: PriceApiService,
+      useClass: useMocks ? MockPriceApiService : PriceApiService,
+    },
+  ],
 })
-export class CryptoPageComponent {
+export class CryptoPageComponent implements OnInit {
+  price$: Observable<string | undefined> = of(undefined);
+  currency$ = new BehaviorSubject<string | undefined>(undefined);
 
+  readonly assets = [
+    { value: 'bitcoin', label: 'Bitcoin' },
+    { value: 'litecoin', label: 'Litecoin' },
+    { value: 'ethereum', label: 'Ethereum' },
+  ];
+
+  constructor(private api: PriceApiService) {}
+
+  ngOnInit() {
+    this.price$ = this.currency$.pipe(
+      mergeMap((currency: string | undefined) => {
+        return currency ? this.api.getPrice(currency) : of(undefined);
+      }),
+      shareReplay(1)
+    );
+  }
+
+  onCryptoSelected(asset: Event) {
+    const currency = (asset as any).target.value;
+    if (this.currency$.value) {
+      this.api.unsubscribe();
+    }
+
+    this.currency$.next(currency);
+  }
 }
