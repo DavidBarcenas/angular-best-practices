0eba94e4041663bc670449837ead2b319ae67efd
David Barcenas <dbarcenas.dev@gmail.com>
2023-07-29 18:20:40 -0600

    feat(crypto): add component

--- signals/src/app/app.component.ts
+++ signals/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { AgeCalculatorComponent } from './age-calculator/age-calculator.component';
+import { CryptoPageComponent } from './crypto/crypto-page/crypto-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, AgeCalculatorComponent],
+  imports: [CommonModule, RouterOutlet, AgeCalculatorComponent, CryptoPageComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
---
+++ signals/src/app/crypto/crypto-page/crypto-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-crypto-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './crypto-page.component.html',
+  styles: [
+  ]
+})
+export class CryptoPageComponent {
+
+}
