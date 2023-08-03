0db614589b937a0a5963528312afe1cc4e90692d
David Barcenas <dbarcenas.dev@gmail.com>
2023-08-03 14:26:51 -0600

    feat(custom-select): add add cdk overlay

--- forms/src/app/custom-select/custom-select/custom-select.component.ts
+++ forms/src/app/custom-select/custom-select/custom-select.component.ts
@@ -1,10 +1,11 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostListener, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { OverlayModule } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-custom-select',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, OverlayModule],
   templateUrl: './custom-select.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -14,4 +15,15 @@ export class CustomSelectComponent {
 
   @Input()
   value: string | null = null;
+
+  @HostListener('click')
+  open(): void {
+    this.isOpen = true;
+  }
+
+  isOpen = false;
+
+  close(): void {
+    this.isOpen = false;
+  }
 }
