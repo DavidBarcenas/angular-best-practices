2d6fc06c64690cc34b48a5ec7024ed9f894f41bd
David Barcenas <dbarcenas.dev@gmail.com>
2023-08-03 17:00:45 -0600

    style(custom-select): add animations

--- forms/src/app/custom-select/custom-select/custom-select.component.ts
+++ forms/src/app/custom-select/custom-select/custom-select.component.ts
@@ -1,6 +1,16 @@
-import { ChangeDetectionStrategy, Component, HostListener, Input } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  HostListener,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { OverlayModule } from '@angular/cdk/overlay';
+import { CdkOverlayOrigin, OverlayModule } from '@angular/cdk/overlay';
+import { animate, AnimationEvent, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-custom-select',
@@ -8,22 +18,63 @@ import { OverlayModule } from '@angular/cdk/overlay';
   imports: [CommonModule, OverlayModule],
   templateUrl: './custom-select.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('dropdown', [
+      state(
+        'void',
+        style({
+          opacity: 0,
+          transform: 'scale(1, 0.8)',
+        })
+      ),
+      state(
+        '*',
+        style({
+          opacity: 1,
+          transform: 'scale(1,1)',
+        })
+      ),
+      transition(':enter', [animate('120ms cubic-bezier(0, 0, 0.2, 1)')]),
+      transition(':leave', [animate('100ms linear')]),
+    ]),
+  ],
 })
-export class CustomSelectComponent {
+export class CustomSelectComponent implements AfterViewInit {
+  @ViewChild('trigger') parent: CdkOverlayOrigin | undefined;
   @Input()
   label = '';
 
   @Input()
   value: string | null = null;
 
+  @Output()
+  readonly opened = new EventEmitter<void>();
+
+  @Output()
+  readonly closed = new EventEmitter<void>();
+
   @HostListener('click')
   open(): void {
     this.isOpen = true;
   }
 
-  isOpen = false;
-
   close(): void {
     this.isOpen = false;
+  }
+
+  isOpen = false;
+  defaultWidth = 'auto';
+
+  ngAfterViewInit() {
+    this.defaultWidth = this.parent?.elementRef.nativeElement.getBoundingClientRect().width + 'px';
+  }
+
+  onPanelAnimationDone({ fromState, toState }: AnimationEvent): void {
+    if (fromState === 'void' && toState === null && this.isOpen) {
+      this.opened.emit();
+    }
+    if (fromState === null && toState === 'void' && !this.isOpen) {
+      this.closed.emit();
+    }
   }
 }
