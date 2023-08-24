5ebb821445526e0b2895f4d3f8db77f97f522409
David Barcenas <dbarcenas.dev@gmail.com>
2023-08-24 14:32:44 -0600

    feat(dynamic-forms): render the form from config

--- forms/src/app/dynamic-forms/dynamic-forms-page/dynamic-forms-page.component.ts
+++ forms/src/app/dynamic-forms/dynamic-forms-page/dynamic-forms-page.component.ts
@@ -1,22 +1,39 @@
 import { ChangeDetectionStrategy, Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Observable, Subject, switchMap } from 'rxjs';
+import { Observable, Subject, switchMap, tap } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { ButtonComponent } from '../../core/button/button.component';
+import { DynamicFormConfig } from '../dynamic-forms.model';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-forms-page',
   standalone: true,
   templateUrl: './dynamic-forms-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CommonModule, ButtonComponent],
+  imports: [CommonModule, ReactiveFormsModule, ButtonComponent],
 })
 export class DynamicFormsPageComponent implements OnInit {
   private http = inject(HttpClient);
   protected formLoadingTrigger = new Subject<'user' | 'company'>();
-  protected formConfig$: Observable<object> | undefined;
+  protected formConfig$: Observable<DynamicFormConfig> | undefined;
+  form!: FormGroup;
 
   ngOnInit(): void {
-    this.formConfig$ = this.formLoadingTrigger.pipe(switchMap((config) => this.http.get(`assets/${config}.form.json`)));
+    this.formConfig$ = this.formLoadingTrigger.pipe(
+      switchMap((config) => this.http.get<DynamicFormConfig>(`assets/${config}.form.json`)),
+      tap(({ controls }) => this.buildForm(controls))
+    );
+  }
+
+  protected onSubmit() {
+    console.log(this.form.getRawValue());
+  }
+
+  private buildForm(controls: DynamicFormConfig['controls']): void {
+    this.form = new FormGroup({});
+    Object.keys(controls).forEach((key) => {
+      this.form?.addControl(key, new FormControl(controls[key].value));
+    });
   }
 }
