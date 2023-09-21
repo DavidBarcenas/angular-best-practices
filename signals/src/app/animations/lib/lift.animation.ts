479526f36d75e2bc98341923db271807f4b2ae55
David Barcenas <dbarcenas.dev@gmail.com>
2023-09-20 21:54:02 -0600

    style(animations): add multiple animations

---
+++ signals/src/app/animations/lib/lift.animation.ts
@@ -0,0 +1,33 @@
+import { animate, animation, style } from '@angular/animations';
+
+export const liftInAnimation = animation(
+  [
+    style({ opacity: '{{ start }}', transform: 'translate({{ startPos }})' }),
+    animate('{{ time }}', style({ opacity: '{{ end }}', transform: 'translate({{ endPos }})' })),
+  ],
+  {
+    params: {
+      time: '500ms ease-out',
+      start: 0,
+      end: 1,
+      startPos: '0, 100px',
+      endPos: '0, 0',
+    },
+  }
+);
+
+export const liftOutAnimation = animation(
+  [
+    style({ opacity: '{{ start }}', transform: 'translate({{ startPos }})' }),
+    animate('{{ time }}', style({ opacity: '{{ end }}', transform: 'translate({{ endPos }})' })),
+  ],
+  {
+    params: {
+      time: '500ms ease-in',
+      start: 1,
+      end: 0,
+      startPos: '0, 0',
+      endPos: '0, -100px',
+    },
+  }
+);
