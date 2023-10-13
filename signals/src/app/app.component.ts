964fa5c816d1a798b8069b52431383bdc5f90824
David Barcenas <dbarcenas.dev@gmail.com>
2023-10-13 17:55:21 -0600

    feat(testing): update initial routing

--- signals/src/app/app.component.ts
+++ signals/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { SeriesComponent } from './animations/series/series.component';
 import { ThemingComponent } from './theming/theming.component';
+import { DashboardComponent } from './testing/components/dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SeriesComponent, ThemingComponent],
+  imports: [CommonModule, RouterOutlet, SeriesComponent, ThemingComponent, DashboardComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
