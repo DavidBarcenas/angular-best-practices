8d3e85be20df9c0fc0fcff4b8a43c3f0f4b49e80
David Barcenas <dbarcenas.dev@gmail.com>
2023-10-18 15:16:30 -0600

    test: update user service methods

--- signals/src/app/testing/services/user.service.ts
+++ signals/src/app/testing/services/user.service.ts
@@ -2,16 +2,21 @@ import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { catchError, map, Observable, of } from 'rxjs';
 import { User } from '../models/user.model';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
   private http = inject(HttpClient);
-  private usersURL = 'https://jsonplaceholder.typicode.com';
+  private messageService = inject(MessageService);
+
+  constructor() {}
+
+  private usersURL = 'api/users';
 
   getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(`${this.usersURL}/users`).pipe(catchError(this.handleError('getUsers', [])));
+    return this.http.get<User[]>(`${this.usersURL}`).pipe(catchError(this.handleError('getUsers', [])));
   }
 
   getUserNo404<Data>(id: number): Observable<User> {
@@ -36,6 +41,22 @@ export class UserService {
       .pipe(catchError(this.handleError<User[]>('searchUsers', [])));
   }
 
+  addUser(user: User): Observable<User> {
+    return this.http.post<User>(`${this.usersURL}/users`, user).pipe(catchError(this.handleError<User>('addUser')));
+  }
+
+  updateUser(user: User): Observable<any> {
+    return this.http
+      .put(`${this.usersURL}/users/${user.id}`, user)
+      .pipe(catchError(this.handleError<any>('updateUser')));
+  }
+
+  deleteUser(user: User | number): Observable<User> {
+    const id = typeof user === 'number' ? user : user.id;
+    const url = `${this.usersURL}/users/${id}`;
+    return this.http.delete<User>(url).pipe(catchError(this.handleError<User>('deleteUser')));
+  }
+
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
       console.error(error);
@@ -45,6 +66,6 @@ export class UserService {
   }
 
   private log(message: string) {
-    //this.messageService.add('HeroService: ' + message);
+    this.messageService.add('HeroService: ' + message);
   }
 }
