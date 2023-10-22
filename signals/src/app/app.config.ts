000558eb68bdeb9801a56807a782a99e9e5e5bae
David Barcenas <dbarcenas.dev@gmail.com>
2023-10-21 19:03:10 -0600

    feat: add provider for http

--- signals/src/app/app.config.ts
+++ signals/src/app/app.config.ts
@@ -3,7 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom([BrowserAnimationsModule]), provideAnimations()],
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom([BrowserAnimationsModule]),
+    provideAnimations(),
+    provideHttpClient(),
+  ],
 };
--- signals/src/app/testing/components/dashboard/dashboard.component.ts
+++ signals/src/app/testing/components/dashboard/dashboard.component.ts
@@ -4,11 +4,12 @@ import { User } from '../../models/user.model';
 import { UserService } from '../../services/user.service';
 import { RouterLink } from '@angular/router';
 import { UserSearchComponent } from '../user-search/user-search.component';
+import { UserComponent } from '../user/user.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, RouterLink, UserSearchComponent],
+  imports: [CommonModule, RouterLink, UserSearchComponent, UserComponent],
   templateUrl: './dashboard.component.html',
   styles: [],
 })
